66ece51f4ec7321fbb8fd6260c220852ad6fc357
Wtz <luo733399955511@163.com>
2026-04-13 19:24:46 +0800

    reconstruct position_embedding

--- Learn/base_model/transformer.py
+++ Learn/base_model/transformer.py
@@ -1,32 +1,35 @@
 import torch
+import torch.nn as nn
 import math
 
-def position_embedding(x: torch.tensor):
-    """
-    x: torch.tensor [batch, seq_length, dim]
-    """
-    assert x.dim() == 3, f"input length should be 3 but get{x.dim()}"
+class PositionEmbeddingLayer(nn.Module):
+    def __init__(self, seq_len, d_model):
+        super().__init__()
+
+        self.position_matrix = torch.zeros(seq_len, d_model)
+        for pos in range(seq_len):
+            for i in range(0, d_model, 2):
+                self.position_matrix[pos][i] = torch.sin(torch.tensor(pos / math.pow(10000, i / d_model)))
+                self.position_matrix[pos][i+1] = torch.cos(torch.tensor(pos / math.pow(10000, i / d_model)))
+
+    def forward(self, x: torch.tensor):
+        """
+        x: torch.tensor [batch, seq_length, dim]
+        """
+        assert x.dim() == 3, f"input length should be 3 but get{x.dim()}"
+        
+        batch = x.shape[0]
+
+        # expand batch
+        position_matrix = self.position_matrix.unsqueeze(0).expand(batch, -1, -1)
+        return x + position_matrix
     
-    position_matrix = torch.zeros_like(x[0])
-
-    batch, seq_len, dim = x.shape
-
-    for pos in range(seq_len):
-        for i in range(0, dim, 2):
-            position_matrix[pos][i] = torch.sin(torch.tensor(pos / math.pow(10000, i / dim)))
-            position_matrix[pos][i+1] = torch.cos(torch.tensor(pos / math.pow(10000, i / dim)))
-
-    # expand batch
-    position_matrix = position_matrix.unsqueeze(0).expand(batch, -1, -1)
-
-    return x + position_matrix
 
 if __name__ == "__main__":
-    x = torch.randn(1, 3, 28)
-    print(x)
+    x = torch.randn(1, 3, 12)
+    batch, seq_len, d_model = x.shape
+    embedding = PositionEmbeddingLayer(x.shape[1], x.shape[2])
+    y = embedding(x)
     print(x.shape)
-    x_with_position = position_embedding(x)
-    print(x_with_position)
-    print(x_with_position.shape)
-
-    print(x_with_position - x)
+    print(y.shape)
+    
